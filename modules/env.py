2c98df4dbc610a5bb1e605f712d4b9aa924b7ab2
gpancardo <ghpancardo@gmail.com>
2025-08-07 23:02:33 -0600

    Get real probabilities and new way to check for requirement compliance

--- modules/env.py
+++ modules/env.py
@@ -1,3 +1,5 @@
+import random as rd
+
 class SimEnv:
     def __init__(self, reward_probs, noise=0.0, epistemic_info=False):
         """
@@ -8,20 +10,21 @@ class SimEnv:
             noise (float): Aditional uncertainty to the reward. Default 0.0.
             epistemic_info (bool): Signals if the environment has to include epistemic clues for the agents that need them.
         """
-        assert len(reward_probs) == 3
-        assert all(0 <= p <= 1 for p in reward_probs)
+        if ((len(reward_probs) == 3) and  (all(0 <= p <= 1 for p in reward_probs))):
 
-        self.n_arms = 3
-        self.reward_probs = reward_probs
-        self.noise = noise
-        self.epistemic_info = epistemic_info
-        
-        # Internal variables to save action histoty if needed.
-        self.round = 0
-        self.history = [] 
+            self.n_arms = 3
+            self.reward_probs = reward_probs
+            self.noise = noise
+            self.epistemic_info = epistemic_info
+            
+            # Internal variables to save action histoty if needed.
+            self.round = 0
+            self.history = [] 
 
-        #Finds optimal arm.
-        self.optimal_arm = self.reward_probs.index(max(self.reward_probs))
+            #Finds optimal arm.
+            self.optimal_arm = self.reward_probs.index(max(self.reward_probs))
+        else:
+            print("Error: The environment must have exactly 3 arms with probabilities between 0 and 1.")
 
     def reset(self):
         #Resets the environment to its initial state.
@@ -29,3 +32,9 @@ class SimEnv:
         self.history = []
         return self.get_observation()
     
+    #Get the true robabilities
+    def get_true_probs(self):
+        return list(self.reward_probs)
+    
+    #Run round
+    def step(self, action):
